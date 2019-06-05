eb9bcbeaa5782f11d36fb6220ad481b8d5bc4830
League Student <league@iMac-91.attlocal.net>
2019-06-04 18:26:41 -0700

    did slots
    :hamburger:

--- src/Slots.java
+++ src/Slots.java
@@ -12,209 +12,122 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Slots implements ActionListener {
-	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton button = new JButton();
-public static void main(String[] args) {
-	
-	Slots slot = new Slots();
-	
-	slot.run();
 
-	
-}
+	JLabel label;
 
-
-public void run() {
-	
-	
-	frame.add(panel);
-	panel.add(button);
-	frame.setVisible(true);
-	
-try {
-JLabel label =	createLabelImage("cherry.jpg");
-panel.add(label);
-} catch (MalformedURLException e) {
-	// TODO Auto-generated catch block
-	
-	e.printStackTrace();
-}
-	
-try {
-	JLabel label1 =createLabelImage("lime .png");
-	panel.add(label1);
-} catch (MalformedURLException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
-		
-	
-try {
-	JLabel label2 =	createLabelImage("orange.png");
-
-panel.add(label2);
-} catch (MalformedURLException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
-
-
-button.setText("Press for a chance to win it all!");
-
-//frame.getDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-
-button.addActionListener((ActionListener) this);
-
-
-Random random = new Random();
-int reel1 = random.nextInt(3);
-try {
-if(reel1 ==1) {
-	JLabel label1 =createLabelImage("lime .png");
-	panel.add(label1);
-	
-}
-
-if(reel1 ==2) {
-	JLabel label1 =createLabelImage("orange .png");
-	panel.add(label1);
-	
-}
-
-
-if(reel1 ==3) {
-	JLabel label1 =createLabelImage("cherry .jpg");
-	panel.add(label1);
-	
-}
-} catch (MalformedURLException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-Random random1 = new Random();
-int reel2 = random1.nextInt(3);
-try {
-if(reel2 ==1) {
-	JLabel label1 =createLabelImage("lime .png");
-	panel.add(label1);
-	
-}
-
-if(reel2 ==2) {
-	JLabel label1 =createLabelImage("orange .png");
-	panel.add(label1);
-	
-}
-
-
-if(reel2 ==3) {
-	JLabel label1 =createLabelImage("cherry .jpg");
-	panel.add(label1);
-	
-}
-} catch (MalformedURLException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
-
-
-
-
-
-
-
-
-
-
-Random random2 = new Random();
-int reel3 = random2.nextInt(3);
-try {
-if(reel3 ==1) {
 	JLabel label1;
 
-		label1 = createLabelImage("lime .png");
-	
-	panel.add(label1);
-	
-}
+	JLabel label2;
 
-if(reel3 ==2) {
-	JLabel label1 =createLabelImage("orange .png");
-	panel.add(label1);
-	
-}
+	public static void main(String[] args) {
 
+		Slots slot = new Slots();
 
-if(reel3 ==3) {
-	JLabel label1 =createLabelImage("cherry .jpg");
-	panel.add(label1);
-	
-}
-} catch (MalformedURLException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
+		slot.run();
 
+	}
 
+	public void run() {
 
+		frame.add(panel);
+		panel.add(button);
+		frame.setVisible(true);
 
+		button.setText("Press for a chance to win it all!");
 
+		label = createLabelImage("cherry.jpg");
 
+		label1 = createLabelImage("lime.png");
 
+		label2 = createLabelImage("orange.png");
 
+		panel.add(label);
+		panel.add(label1);
+		panel.add(label2);
+		frame.pack();
+//frame.getDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 
+		button.addActionListener((ActionListener) this);
 
+	}
 
+	private JLabel createLabelImage(String fileName) {
+		URL imageURL = getClass().getResource(fileName);
+		if (imageURL == null) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+		Icon icon = new ImageIcon(imageURL);
+		JLabel imageLabel = new JLabel(icon);
+		return imageLabel;
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		panel.remove(label);
+		Random random1 = new Random();
 
+		int reel2 = random1.nextInt(3);
 
+		if (reel2 == 1) {
+			label = createLabelImage("lime.png");
 
+		}
 
+		if (reel2 == 2) {
+			label = createLabelImage("orange.png");
 
+		}
 
+		if (reel2 == 0) {
+			label = createLabelImage("cherry.jpg");
 
+		}
+		panel.add(label);
+		panel.remove(label1);
+		Random random = new Random();
+		int reel1 = random1.nextInt(3);
 
+		if (reel2 == 1) {
+			label1 = createLabelImage("lime.png");
 
+		}
 
+		if (reel2 == 2) {
+			label1 = createLabelImage("orange.png");
 
+		}
 
+		if (reel2 == 0) {
+			label1 = createLabelImage("cherry.jpg");
 
-frame.pack();
-	
+		}
+		panel.add(label1);
+		panel.remove(label2);
+		Random random2 = new Random();
+		int reel3 = random2.nextInt(3);
 
-}
+		if (reel3 == 1) {
 
-private JLabel createLabelImage(String fileName) throws MalformedURLException{
-    URL imageURL = getClass().getResource(fileName);
-if (imageURL == null){
-	System.err.println("Could not find image " + fileName);
-	return new JLabel();
-}
-Icon icon = new ImageIcon(imageURL);
-JLabel imageLabel = new JLabel(icon);
-return imageLabel;
-}
+			label2 = createLabelImage("lime.png");
 
+		}
 
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	
-	
-}
+		if (reel3 == 2) {
+			label2 = createLabelImage("orange.png");
 
+		}
+
+		if (reel3 == 0) {
+			label2 = createLabelImage("cherry.jpg");
+
+		}
+		panel.add(label2);
+		frame.pack();
+
+	}
 }
